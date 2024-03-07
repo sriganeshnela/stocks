8dbccd4d22740c93e353ee0c4b4e242eefe4a7c5
sriganeshnela <161318276+sriganeshnela@users.noreply.github.com>
2024-03-07 12:51:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var score=0;
+
+function decrease(){
+    score=score-5;
+    document.getElementById("box").textContent=score;
+}
+function increase(){
+    score=score+5;
+    document.getElementById("box").textContent=score;
+}
